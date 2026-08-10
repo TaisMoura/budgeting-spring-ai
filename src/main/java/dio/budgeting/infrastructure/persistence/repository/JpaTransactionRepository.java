a08b95346695c26acbdc0c6702f99133f64d39a3
TaisMoura <tais.moura@pepsico.com>
2026-08-10 19:25:10 +0000

    feat: add transaction listing repository method

--- src/main/java/dio/budgeting/infrastructure/persistence/repository/JpaTransactionRepository.java
+++ src/main/java/dio/budgeting/infrastructure/persistence/repository/JpaTransactionRepository.java
@@ -7,9 +7,11 @@ import dio.budgeting.infrastructure.persistence.entity.TransactionEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Repository
 public class JpaTransactionRepository implements TransactionRepository {
+
     private final TransactionEntityRepository transactionEntityRepository;
 
     public JpaTransactionRepository(TransactionEntityRepository transactionEntityRepository) {
@@ -26,6 +28,15 @@ public class JpaTransactionRepository implements TransactionRepository {
     public List<Transaction> findAllByCategory(Category category) {
         return transactionEntityRepository.findAllByCategory(category)
                 .stream()
+                .map(TransactionEntity::toDomain)
+                .toList();
+    }
+
+    @Override
+    public List<Transaction> findAll() {
+        return StreamSupport.stream(
+                        transactionEntityRepository.findAll().spliterator(),
+                        false)
                 .map(TransactionEntity::toDomain)
                 .toList();
     }
